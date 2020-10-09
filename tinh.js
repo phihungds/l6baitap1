cfd4fd556da7cf670fea138c0891d4538dade8b0
Phi Hung <phihunds1@gmail.com>
2020-10-09 21:54:22 +0700

    phase 2

--- tinh.js
+++ tinh.js
@@ -2,27 +2,36 @@ function tong() {
     let soThuNhat = Number(document.getElementById("soThuNhat").value)
     let soThuHai = Number(document.getElementById("soThuHai").value)
     let tong = soThuNhat + soThuHai
-    let phatNgon = "Tổng hai số là: " + tong
+    let phatNgon = soThuNhat +  " + " + soThuHai + "= " + tong
     document.getElementById("phatNgon").innerText = phatNgon
+    document.getElementById("ketQua").value = tong
 }
 function hieu() {
     let soThuNhat = Number(document.getElementById("soThuNhat").value)
     let soThuHai = Number(document.getElementById("soThuHai").value)
     let hieu = soThuNhat - soThuHai
-    let phatNgon = "Hiệu hai số là: " + hieu
+    let phatNgon = soThuNhat +  " - " + soThuHai + "= " + hieu
     document.getElementById("phatNgon").innerText = phatNgon
+    document.getElementById("ketQua").value = hieu
 }
 function tich() {
     let soThuNhat = Number(document.getElementById("soThuNhat").value)
     let soThuHai = Number(document.getElementById("soThuHai").value)
     let tich = soThuNhat * soThuHai
-    let phatNgon = "Tích hai số là: " + tich
+    let phatNgon = soThuNhat +  " x " + soThuHai + "= " + tich
     document.getElementById("phatNgon").innerText = phatNgon
+    document.getElementById("ketQua").value = tich
 }
 function thuong() {
     let soThuNhat = Number(document.getElementById("soThuNhat").value)
     let soThuHai = Number(document.getElementById("soThuHai").value)
     let thuong = soThuNhat / soThuHai
-    let phatNgon = "Thương hai số là: " + thuong
+    let phatNgon = soThuNhat +  " : " + soThuHai + "= " + thuong
     document.getElementById("phatNgon").innerText = phatNgon
+    document.getElementById("ketQua").value = thuong
+}
+function use() {
+    document.getElementById("soThuNhat").value = document.getElementById("ketQua").value
+    document.getElementById("soThuHai").value = ""
+    document.getElementById("soThuHai").focus()
 }
